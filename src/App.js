53a851b58c316c4a7fe9ad96f32373b2bd0ffdbf
Ashutosh Mittal <ashutoshmittal@gmail.com>
2020-09-11 02:18:17 -0700

    first commit with board up and running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [nextMove, setNextMove] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const handleSquareClick = (idx) => {
+    let copyBoard = [...board];
+
+    // Check if square was already clicked
+    if (copyBoard[idx] || winner) return;
+
+    // update the array with move to make
+    copyBoard[idx] = nextMove;
+
+    // call the function to find if Winning move has been made
+    setWinner(calculateWinner(copyBoard));
+
+    setNextMove(nextMove === "X" ? "O" : "X");
+    setBoard(copyBoard);
+  };
+
+  const calculateWinner = (board) => {};
+
+  const handleResetClick = () => {
+    setNextMove("X");
+    setBoard(Array(9).fill(null));
+    setWinner(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="board">
+        {board.map((item, idx) => (
+          <Square
+            className="square"
+            key={idx}
+            value={item}
+            onClick={() => handleSquareClick(idx)}
+          />
+        ))}
+      </div>
+      {winner ? (
+        <div className="details">Winner - {winner} </div>
+      ) : (
+        <div className="details"> Next Move - {nextMove} </div>
+      )}
+      <button className="reset" onClick={() => handleResetClick()}>
+        Reset Board
+      </button>
     </div>
+  );
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
 }
 
